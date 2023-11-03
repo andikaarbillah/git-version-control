ac1ca8d22eabe5fe28c8b9395ca2319f48889bda
andikaarbillah <chrandika3@gmail.com>
2023-11-03 09:13:55 +0700

    [ADD] add comment main.go

--- main.go
+++ main.go
@@ -3,3 +3,17 @@ package main
 func main(){
 	
 }
+
+//message
+/*
+- git init
+- git status
+- rm -rf
+- git add . / git add name_file
+- git commit -m "message"
+- example message
+		[FEATURE] untuk penambahan fitur
+		[IMPROVE] jika ada perubahan
+		[FIX] untuk project yang fix
+		[MERGE]
+*/
