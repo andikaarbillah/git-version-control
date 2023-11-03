403b477418a3f08afb5d618878a102638ec3c22c
andikaarbillah <chrandika3@gmail.com>
2023-11-03 09:20:01 +0700

    add comment

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ func main(){
 - rm -rf
 - git add . / git add name_file
 - git commit -m "message"
+- git commit -am "message"
 - example message
 		[FEATURE] untuk penambahan fitur
 		[IMPROVE] jika ada perubahan
 		[FIX] untuk project yang fix
 		[MERGE]
+- git log untuk melihat riwayat git
+- git log --oneline
 */
